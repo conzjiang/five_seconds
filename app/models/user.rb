001a62fad0bf9d6f6f2eeb10298474bfb2ab9472
Constance Jiang <conzjiang@gmail.com>
2015-02-08 19:33:33 -0500

    Create sessions table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   enum status: ["Employed", "Actively Looking", "Doing My Own Thing"]
 
   validates :first_name, :last_name, :password_digest, presence: true
+
+  has_many :sessions
 end
