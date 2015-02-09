22f0e022aa0b0d804cdbd83b2efc264ca5bb0497
Constance Jiang <conzjiang@gmail.com>
2015-02-09 00:07:00 -0500

    Sign out.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,21 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :signed_in?
 
   private
+  def current_session
+    @session ||= Session.find_by(session_token: session[:token])
+  end
+
   def current_user
-    @current_user ||= Session.find_by(session_token: session[:token]).user
+    @current_user ||= current_session.user
   end
 
   def sign_in!(user)
     session[:token] = user.set_session_token!
+  end
+
+  def sign_out!
+    current_session.destroy!
+    session[:token] = nil
   end
 
   def signed_in?
