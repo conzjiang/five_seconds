89868fb1bb417049367c8469151cb69a89ef463d
Constance Jiang <conzjiang@gmail.com>
2015-02-08 23:31:41 -0500

    User specs.

--- app/models/session.rb
+++ app/models/session.rb
@@ -7,6 +7,6 @@ class Session < ActiveRecord::Base
 
   private
   def generate_session_token
-    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,9 +8,26 @@ RSpec.describe User do
   it { should validate_presence_of(:password_digest) }
   it { should validate_length_of(:password).is_at_least(6) }
   it { should validate_confirmation_of(:password) }
+  it { should have_many(:sessions) }
 
   it "doesn't test password confirmation on update" do
     user.password_confirmation = nil
     expect(user).to be_valid
+  end
+
+  it "sets password digest on submission of password" do
+    user = build(:user)
+    expect(user.password_digest).not_to be_nil
+  end
+
+  describe "#set_session_token!" do
+    it "creates a session" do
+      user.set_session_token!
+      expect(user.sessions).not_to be_empty
+    end
+
+    it "returns session token" do
+      expect(user.set_session_token!).to eq(user.sessions.first.session_token)
+    end
   end
 end
